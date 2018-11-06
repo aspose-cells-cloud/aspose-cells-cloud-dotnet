1c03cb0af3e0443c6c2a847a93abd4f86f00f73b
Roy Wang <roy.wang@aspose.com>
2018-11-06 16:40:28 +0800

    Fixed test case.

--- Aspose.Cells.Cloud.SDK.Test/Api/OAuthApiTests.cs
+++ Aspose.Cells.Cloud.SDK.Test/Api/OAuthApiTests.cs
@@ -72,8 +72,8 @@ namespace Aspose.Cells.Cloud.SDK.Test
         {
             // TODO uncomment below to test the method and replace null with proper value
             string grantType = "client_credentials";
-            string clientId = "66164C51-693E-4904-A121-545961673EC1";
-            string clientSecret = "536e76768419db9585afdd37bb5f7533";
+            string clientId = "xxxxx-xxxx-xxxx-xxxx-xxxxxxx";
+            string clientSecret = "xxxxxxxxxxxxxxxxxxx";
             var response = instance.OAuthPost(grantType, clientId, clientSecret);
             Assert.IsInstanceOf<AccessTokenResponse>(response, "response is AccessTokenResponse");
             Assert.IsNotNull(response.AccessToken);
