42f2bced8da2cdfdadbf936e5187a56d9988f168
roy wang <roy.wang@aspose.com>
2022-11-15 20:17:23 -0600

    Release Aspose.Cells Cloud SDK 22.11

--- Aspose.Cells.Cloud.SDK.Test/Custom/CellsCustomTests.cs
+++ Aspose.Cells.Cloud.SDK.Test/Custom/CellsCustomTests.cs
@@ -54,6 +54,37 @@ namespace Aspose.Cells.Cloud.SDK.Test
                 fileStream.Close();
             }
         }
+        [Test]
+        public void CELLSCLOUD17020Test()
+        {
+            // TODO uncomment below to test the method and replace null with proper value
+            instance.CellsWorkbookPutWorkbookCreate("CELLSCLOUD17020Test.xlsx", null, null, null, "Custom");
+            instance.CellsPostWorksheetCellSetValue("CELLSCLOUD17020Test.xlsx", "Sheet1", "A1", "10", "int", null, "Custom");
+            instance.CellsPostWorksheetCellSetValue("CELLSCLOUD17020Test.xlsx", "Sheet1", "B1", "10", "int", null, "Custom");
+            instance.CellsPostWorksheetCellSetValue("CELLSCLOUD17020Test.xlsx", "Sheet1", "C1", "10", "int", null, "Custom");
+            instance.CellsPostWorksheetCellSetValue("CELLSCLOUD17020Test.xlsx", "Sheet1", "D1", "10", "int", null, "Custom");
+            instance.CellsPostWorksheetCellSetValue("CELLSCLOUD17020Test.xlsx", "Sheet1", "A2", null, null, "=Sum(A1,B1,C1,D1)", "Custom");
+            instance.CellsWorksheetsPostRenameWorksheet("CELLSCLOUD17020Test.xlsx", "Sheet1", "Start", "Custom");
+            instance.CellsWorksheetsPutAddNewWorksheet("CELLSCLOUD17020Test.xlsx", "Other data", null, null, "Custom");
+            Comment comment = new Comment();
+            comment.Note = "TEST";
+            instance.CellsWorksheetsPutWorksheetComment("CELLSCLOUD17020Test.xlsx", "Start", "A1", comment, "Custom");
+            comment.Link = new Link();
+            comment.Link.Href = "CELLSCLOUD17020Test.xlsx/worksheets/Start/comments/A2/";
+            comment.Link.Rel = "self";
+            comment.AutoSize = true;
+            comment.Author = "Wo_0NDeR";
+            comment.IsVisible = true;
+            comment.TextOrientationType = "NoRotation";
+            comment.TextHorizontalAlignment = "Left";
+            comment.Height = 10;
+            comment.Width = 10;
+            comment.TextVerticalAlignment = "Top";
+            comment.HtmlNote = "This is a note from Wo_0NDeR";
+            comment.Note = "This is a note from Wo_0NDeR";
+            comment.CellName = "A2";
+            instance.CellsWorksheetsPostWorksheetComment("CELLSCLOUD17020Test.xlsx", "Start", "A1", comment, "Custom");
+        }
     }
 
 }
