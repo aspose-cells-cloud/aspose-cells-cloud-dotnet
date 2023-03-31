695926fb534ac5fe81e41ab23e7f957541e16759
roy.wang <roy.wang@aspose.com>
2023-03-31 01:36:55 -0700

    Update project file and add sign function in Pack_NugetPackage.ps1

--- Pack_NugetPackage.ps1
+++ Pack_NugetPackage.ps1
@@ -8,10 +8,11 @@ if( $FileVersion.Length -eq 2 ){
 }else{
     $version=$fileVersion[0],$fileVersion[1],$fileVersion[2] -join('.')
 }
-$TempPackFile="Aspose.Cells.Cloud.SDK.$version.nupkg"
-$PackFile="packages\Aspose.Cells-Cloud.$version.nupkg" 
+$PackFile="Aspose.Cells-Cloud.$version.nupkg" 
 
 Write-Output "Pack Aspose.Cells Cloud SDK $version For Net."
 dotnet pack --output .\
-Write-Output "Move-Item  -Path $TempPackFile -Destination  $PackFile"
-Move-Item  -Path  "$TempPackFile" -Destination  "$PackFile"
+dotnet nuget sign  --hash-algorithm  sha256 --certificate-path aspose.pfx  --certificate-password f27Hp99Ds3  --timestamper http://timestamp.digicert.com $PackFile
+dotnet nuget verify $PackFile
+Write-Output "Move-Item  -Path $PackFile -Destination  packages/$PackFile"
+Move-Item  -Path  "$PackFile" -Destination  "packages/$PackFile"
