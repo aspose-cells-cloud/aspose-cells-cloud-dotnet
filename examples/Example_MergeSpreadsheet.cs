139bd0892765e392ec41b58f35ae86bcc61c1adb
roy.wang <roy.wang@aspose.com>
2025-08-26 11:43:06 +0800

    Add Merge Spreadsheet examples.

---
+++ examples/Example_MergeSpreadsheet.cs
@@ -0,0 +1,36 @@
+public static class Example_MergeSpreadsheet
+{
+    public static void Run()
+    {
+        MergedLocalFiles();
+        MergeRemoteSpreadsheet();
+    }
+
+    private static void MergedLocalFiles()
+    {
+        // Get your Client ID and Client Secret from https://dashboard.aspose.cloud (free registration is required).
+        var cellsApi = new Aspose.Cells.Cloud.SDK.Api.CellsApi(System.Environment.GetEnvironmentVariable("ProductClientId"), System.Environment.GetEnvironmentVariable("ProductClientSecret"));
+        // Build merge request parameters 
+        var request = new Aspose.Cells.Cloud.SDK.Request.MergeSpreadsheetsRequest();
+        IDictionary<string, System.IO.Stream> mapFiles = new Dictionary<string, System.IO.Stream>();
+        mapFiles.Add("Book1.xlsx", File.OpenRead(@"c:\testdata\Book1.xlsx"));
+        mapFiles.Add("Book2.xlsx", File.OpenRead(@"c:\testdata\Book2.xlsx"));
+        request.Spreadsheet = mapFiles;
+        request.outFormat = "pdf";
+        cellsApi.MergeSpreadsheets(request, "MergedResultFile.pdf");
+    }
+
+    private static void MergeRemoteSpreadsheet()
+    {
+        // Get your Client ID and Client Secret from https://dashboard.aspose.cloud (free registration is required).
+        var cellsApi = new Aspose.Cells.Cloud.SDK.Api.CellsApi(System.Environment.GetEnvironmentVariable("ProductClientId"), System.Environment.GetEnvironmentVariable("ProductClientSecret"));
+        // Build merge request parameters 
+        var request = new Aspose.Cells.Cloud.SDK.Request.MergeRemoteSpreadsheetRequest();
+       
+        request.name = "Book1.xlsx";
+        request.folder = "RemoteFolder1";
+        request.mergedSpreadsheet = "RemoteFolder2/Book2.xlsx";
+        request.outFormat = "pdf";
+        cellsApi.MergeRemoteSpreadsheet(request, "MergedResultOutPutToLocalFile.pdf");
+    }
+}
